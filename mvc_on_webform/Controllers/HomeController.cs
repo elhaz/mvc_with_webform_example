8f11819c12952c8fd5b3553457bd2e0010151d5c
Xehoon Han <sa2622@naver.com>
2023-07-06 18:10:25 +0900

    mvc - 홈컨트롤러 추가.
    index action 및 webform용 action 추가

---
+++ mvc_on_webform/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_on_webform.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Site1() {
+            Server.Transfer("~/WebForm/Site1/WebForm1.aspx");
+            return View();
+        }
+    }
+}
